2856672e4cadeae045584d0ae5b5eb89e4a47a27
Qabas al ani <doctorqa@yahoo.com>
2021-11-02 21:07:29 -0400

    added the Comic.js that contains the comic schema

---
+++ server/models/Comic.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const comicSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String
+  },
+  image: {
+    type: String
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    min: 0,
+    default: 0
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true
+  }
+});
+
+const Comic = mongoose.model('Comic', comicSchema);
+
+module.exports = Comic;
