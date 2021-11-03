99732bbf2d476c1b7fa342a97bd5bfa1bca68f3e
Jimmy Simpson <simpsonjd12@gmail.com>
2021-11-02 20:55:30 -0400

    installed Material UI and made first component

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import { Button } from '@mui/material';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <>
+<div className="App">
+    <Button onClick={()=>alert("Just Kidding")} variant="outlined" >
+  Click me to see your completed COMIC SPACE project 
+    </Button>
     </div>
+  </>
   );
 }
 
