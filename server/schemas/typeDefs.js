d7d5a25b9a565e27974b3e677ffd5d216243a1cb
Qabas al ani <doctorqa@yahoo.com>
2021-11-02 21:39:37 -0400

    added the typeDefs.js which contains the required arguments and types

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,60 @@
 const { gql } = require('apollo-server-express');
 
-const typeDefs = gql``;
+const typeDefs = gql`
+  type Category {
+    _id: ID
+    name: String
+  }
+
+  type Comic {
+    _id: ID
+    name: String
+    description: String
+    image: String
+    quantity: Int
+    price: Float
+    category: Category
+  }
+
+  type Order {
+    _id: ID
+    purchaseDate: String
+    comics: [Comic]
+  }
+
+  type User {
+    _id: ID
+    firstName: String
+    lastName: String
+    email: String
+    orders: [Order]
+  }
+
+  type Checkout {
+    session: ID
+  }
+
+  type Auth {
+    token: ID
+    user: User
+  }
+
+  type Query {
+    categories: [Category]
+    comics(category: ID, name: String): [Comic]
+    comic(_id: ID!): Comic
+    user: User
+    order(_id: ID!): Order
+    checkout(comics: [ID]!): Checkout
+  }
+
+  type Mutation {
+    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+    addOrder(comics: [ID]!): Order
+    updateUser(firstName: String, lastName: String, email: String, password: String): User
+    updateComic(_id: ID!, quantity: Int!): Comic
+    login(email: String!, password: String!): Auth
+  }
+`;
 
 module.exports = typeDefs;
