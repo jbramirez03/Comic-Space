1d0f8371568c29bbca5d5c7f48f3e57451f52fbb
Beardo Mattix <christopher.mattix@gmail.com>
2021-11-03 19:03:29 -0400

    profile page and npms

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
-// import logo from "./logo.svg";
+
 import "./App.css";
+
 import SignInSide from "./components/SignInSide";
-// import SignUp from "./components/SignUp";
+
+import SignUp from "./components/SignUp";
 
 function App() {
   return <SignInSide />;
---
+++ client/src/components/pages/Profile.js
@@ -0,0 +1,174 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Button from "@mui/material/Button";
+// import CameraIcon from "@mui/icons-material/PhotoCamera";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Link from "@mui/material/Link";
+import ImportContactsIcon from "@mui/icons-material/ImportContacts";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Comic Space
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const theme = createTheme();
+
+export default function Profile() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <ImportContactsIcon sx={{ mr: 2 }} />
+          <Typography variant="h6" color="inherit" noWrap>
+            Your Collection
+          </Typography>
+          <Button
+            size="small"
+            variant="contained"
+            sx={{ ml: 5, bgcolor: "white", color: "#531c28" }}
+          >
+            Home
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <main style={{ backgroundColor: "#531c28" }}>
+        {/* Hero unit */}
+        <Box
+          sx={{
+            boxShadow: 2,
+            bgcolor: "#385059",
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              gutterBottom
+              style={{ color: "white" }}
+            >
+              Profile
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              style={{ color: "white" }}
+              paragraph
+            >
+              Something short and leading about the collection below—its
+              contents, the creator, etc. Make it short and sweet, but not too
+              short so folks don&apos;t simply skip over it entirely.
+            </Typography>
+            <Stack
+              sx={{ pt: 4 }}
+              direction="row"
+              spacing={2}
+              justifyContent="center"
+            >
+              <Button variant="contained">Message Me</Button>
+              <Button variant="outlined" style={{ color: "white" }}>
+                Secondary action
+              </Button>
+            </Stack>
+          </Container>
+        </Box>
+        <Container sx={{ py: 8, bgcolor: "#531c28" }} maxWidth="md">
+          <Typography
+            component="h1"
+            variant="h2"
+            align="center"
+            gutterBottom
+            style={{ color: "white" }}
+          >
+            Collection
+          </Typography>
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card
+                  sx={{
+                    boxShadow: 2,
+                    backgroundColor: "#385059",
+                    height: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
+                  <CardMedia
+                    component="img"
+                    sx={
+                      {
+                        // 16:9
+                        //   pt: "56.25%",
+                      }
+                    }
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToHQhsZXsVSBRZSkkzVRfeCKuz0ADr-vFfMw&usqp=CAU"
+                    alt="random"
+                  />
+                  <CardContent sx={{ flexGrow: 1, color: "white" }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to describe
+                      the content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" variant="contained">
+                      View
+                    </Button>
+                    <Button size="small" variant="contained">
+                      Buy
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      {/* Footer */}
+      <Box sx={{ bgcolor: "background.paper", p: 6 }} component="footer">
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="text.secondary"
+          component="p"
+        >
+          Something here to give the footer a purpose!
+        </Typography>
+        <Copyright />
+      </Box>
+      {/* End footer */}
+    </ThemeProvider>
+  );
+}
