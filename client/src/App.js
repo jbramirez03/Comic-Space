e2c53b0c158b2685219350adfead55a2700c6a96
Beardo Mattix <christopher.mattix@gmail.com>
2021-11-03 19:58:45 -0400

    added react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,30 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { Provider } from "reacrt-redux";
 import "./App.css";
-
+import { Store } from "./utils/store";
 import SignInSide from "./components/SignInSide";
 
 import SignUp from "./components/SignUp";
+import Profile from "./components/pages/Profile";
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
@@ -31,7 +33,6 @@ const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
-
 
 function App() {
   return (
