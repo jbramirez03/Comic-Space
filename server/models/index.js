a64de70eef542f7c398868a60bc1465a6c6c03bf
Qabas al ani <doctorqa@yahoo.com>
2021-11-02 21:17:54 -0400

    added the index.js that contains the imports and the exports for the models

---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const User = require('./User');
+const Comic = require('./Comic');
+const Category = require('./Category');
+const Order = require('./Order');
+
+module.exports = { User, Comic, Category, Order };
