37c8c15d708554bd31a5cbbcf20dc64ab73f73fd
temptado <jason1287712@gmail.com>
2021-11-03 19:03:59 -0400

    added apollo client to front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,41 @@
 import React from "react";
-// import logo from "./logo.svg";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import "./App.css";
 import SignInSide from "./components/SignInSide";
 // import SignUp from "./components/SignUp";
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+
 function App() {
-  return <SignInSide />;
+  return (
+    <ApolloProvider client={client}>
+      <SignInSide />
+    </ApolloProvider>
+  );
 }
 
 export default App;
