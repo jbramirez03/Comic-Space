56759a0786a74dbdda975ce0445553b16fd172c1
temptado <jason1287712@gmail.com>
2021-11-06 11:48:56 -0400

    signup test

--- client/src/components/pages/SignUpTest.js
+++ client/src/components/pages/SignUpTest.js
@@ -5,17 +5,65 @@ import { ADD_USER } from '../../utils/mutations';
 import Auth from '../../utils/auth';
 
 const SignUpTest = () => {
-    const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+    const [userFormData, setUserFormData] = useState({ firstName: '', lastName: '', email: '', password: '' });
 
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setUserFormData({ ...userFormData, [name]: value });
+    };
+
+    const [addUser] = useMutation(ADD_USER);
+
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const { data } = await addUser({
+                variables: { ...userFormData },
+            });
+
+            Auth.login(data.addUser.token);
+        } catch (err) {
+            console.error(err);
+        }
+
+        setUserFormData({
+            firstName: '',
+            lastName: '',
+            email: '',
+            password: '',
+        });
+    }
 
     return (
         <div>
-            <form action="submit">
-                <input type="text" />
-                <input type="text" />
-                <input type="text" />
-                <input type="email" />
-                <input type="password" />
+            <form action="submit" onSubmit={handleFormSubmit}>
+                <input type="text"
+                    placeholder='first name..'
+                    name='firstName'
+                    onChange={handleInputChange}
+                    value={userFormData.firstName}
+                    required />
+                <input type="text"
+                    placeholder='last name..'
+                    name='lastName'
+                    onChange={handleInputChange}
+                    value={userFormData.lastName}
+                />
+                <input type="email"
+                    placeholder='email...'
+                    name='email'
+                    onChange={handleInputChange}
+                    value={userFormData.email}
+                />
+                <input type="password"
+                    placeholder='password...'
+                    name='password'
+                    onChange={handleInputChange}
+                    value={userFormData.password}
+                />
+                <button>Signup</button>
             </form>
         </div>
     )
