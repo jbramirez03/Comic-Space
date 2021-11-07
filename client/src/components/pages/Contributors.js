76fd6ab64b293b0e14e7f326f1ef3287d5821a2e
Qabas al ani <doctorqa@yahoo.com>
2021-11-07 12:24:25 -0500

    added box shadow to the flip book pages and css

--- client/src/components/pages/Contributors.js
+++ client/src/components/pages/Contributors.js
@@ -1,9 +1,20 @@
 import HTMLFlipBook from 'react-pageflip';
 
 const Contributors = () => {
+
+    const styles = {
+        boxShadow: '0 0 8px 8px gray',
+        display: 'block',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginTop: '2%',
+        marginBottom: 'auto',
+        width: '40%',
+    }
+
     return (
-        <HTMLFlipBook width={300} height={400} drawShadow={true} >
-        <div style={{backgroundColor: 'lightBlue'}} className="demoPage">Page 1</div>
+        <HTMLFlipBook width={300} height={400} drawShadow={true} style={styles} >
+        <div  className="demoPage">Page 1</div>
         <div className="demoPage">Page 2</div>
         <div className="demoPage">Page 3</div>
         <div className="demoPage">Page 4</div>
