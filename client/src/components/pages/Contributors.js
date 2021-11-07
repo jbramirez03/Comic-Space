25c81b9f76c1dc4b377b9b8597ab5fc56fd76139
Qabas al ani <doctorqa@yahoo.com>
2021-11-07 16:03:30 -0500

    added a first page for the book

--- client/src/components/pages/Contributors.js
+++ client/src/components/pages/Contributors.js
@@ -17,6 +17,32 @@ const Contributors = () => {
 
   return (
     <HTMLFlipBook width={350} height={450} drawShadow={true} style={styles}>
+
+<div className="demoPage">
+        <div className="info">
+          <img className="avatar" src={chris} alt="chris" />
+          <h3>Chris Mattix</h3>
+        </div>
+      </div>
+      <div className="demoPage">
+        <div className="info">
+          <h3>About Me</h3>
+          <p>
+            Hello and thanks for checking out my portfolio! My name is Chris
+            Mattix and I am currently a student in the UNC Full Stack Developer
+            Bootcamp. I am a former teacher who spent 12 years in education
+            before deciding it was time to move on to a new career where I can
+            put my problem-solving skills to the test. I am originally from
+            Montana, and miss my home state everyday. I have lived in North
+            Carolina for the past 6 years, and truly love it...even if the
+            mountains here don't have the same majesty as the ones in Montana.
+          </p>
+        </div>
+      </div>
+
+
+
+
       <div className="demoPage">
         <div className="info">
           <img className="avatar" src={chris} alt="chris" />
@@ -56,7 +82,6 @@ const Contributors = () => {
         </div>
       </div>
 
-      
       <div className="demoPage">
         <div className="info">
           <img className="avatar" src={jason} alt="jason" />
@@ -67,27 +92,32 @@ const Contributors = () => {
         <div className="info">
           <h3>About Me</h3>
           <p>
-          Full-Stack Web Developer, a graduate of `The Coding Bootcamp At UNC-Chapel Hill` with a special appreciation for back-end projects and a life-long dedication to learning. Determined to improve in all aspects of life and known amongst peers for having an eye for detail and being skilled at problem-solving no matter the complexity of the project.
+            Full-Stack Web Developer, a graduate of `The Coding Bootcamp At
+            UNC-Chapel Hill` with a special appreciation for back-end projects
+            and a life-long dedication to learning. Determined to improve in all
+            aspects of life and known amongst peers for having an eye for detail
+            and being skilled at problem-solving no matter the complexity of the
+            project.
           </p>
         </div>
       </div>
 
-
       <div className="demoPage">
         <div className="info">
-          <img className="avatar" src={jason} alt="jason" />
-          <h3>Jason Barrera</h3>
+          <img className="avatar" src={qabas} alt="qabas" />
+          <h3>Qabas Al Ani</h3>
         </div>
       </div>
       <div className="demoPage">
         <div className="info">
           <h3>About Me</h3>
           <p>
-          Full-Stack Web Developer, a graduate of `The Coding Bootcamp At UNC-Chapel Hill` with a special appreciation for back-end projects and a life-long dedication to learning. Determined to improve in all aspects of life and known amongst peers for having an eye for detail and being skilled at problem-solving no matter the complexity of the project.
+            Full Stack Web Developer, love developing apps and websites with a
+            focus on mobile-first design & graduate of the UNC Chapel Hill
+            Coding Boot Camp
           </p>
         </div>
       </div>
-
 
       <div className="demoPage">Page 4</div>
     </HTMLFlipBook>
