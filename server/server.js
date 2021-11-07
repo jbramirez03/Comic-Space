521b21c221f40c7e493c0abd0112e96b8c02e914
temptado <jason1287712@gmail.com>
2021-11-06 20:26:32 -0400

    fixed issue with auth by adding the context middleware in the server.js file

--- server/server.js
+++ server/server.js
@@ -3,12 +3,14 @@ const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
 const db = require('./config/connection');
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
     typeDefs,
     resolvers,
+    context: authMiddleware,
 });
 
 server.applyMiddleware({ app });
