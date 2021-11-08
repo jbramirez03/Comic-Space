8faf089c74fc052291a7f073db815885179b7ad2
Jimmy Simpson <simpsonjd12@gmail.com>
2021-11-08 18:33:30 -0500

    changes to forum pages to get everone updated

--- client/src/components/pages/Forum.js
+++ client/src/components/pages/Forum.js
@@ -4,6 +4,7 @@ import TextField from '@mui/material/TextField';
 import { Container } from '@mui/material';
 import Button from "@mui/material/Button";
 import ForumReply from '../ForumReply';
+import { useState } from 'react';
 
 const divBox = {
     width: "100%",
@@ -12,7 +13,7 @@ const divBox = {
     display: "block",
   
 }
-const message = {
+const messageStyle = {
 width: "100%",
 marginTop: "3%",
 alignItems: "center"
@@ -21,27 +22,52 @@ const submitButton = {
     marginTop:"2%",
   marginLeft: "90%"
 }
+
+
 const Forum = () => {
+    // const [body, setBody] = useState('');
+
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();
+    //     const message = {body.value};
+
+    //     fetch('http://localhost:3000/forum', {
+    //         method: 'POST', 
+    //         body: JSON.stringify(message)
+    //     }).then(() => 
+    //     console.log("new thread made"))
+    // }
     return (
+        
         <div style={divBox}>
-            <div><Container style={message}>
+            <div><Container style={messageStyle}>
                 Messages
             </Container></div>
             
-        <Container style={message}>
+        <Container style={messageStyle}>
         <TextField 
-     fullWidth
-         id="outlined-multiline-static"
-         label=""
+         fullWidth
+         id="outlined-multiline-static body"
+         label="start thread"
          multiline
          rows={3}
-         defaultValue="Start a conversation"
+         defaultValue=""
          >
         </TextField>
         <Box style={submitButton}><Button variant="contained">Submit</Button>
 </Box>
         </Container>
-        <ForumReply></ForumReply>
+        <ForumReply> <TextField 
+     fullWidth
+         id="outlined-multiline-static reply"
+         label=""
+         multiline
+         rows={1.5}
+         defaultValue="Reply to this thread..."
+         >
+        </TextField></ForumReply>
+
+       
         </div>
     )
 }
