c24f04837bb8a066066905132370d708cb8aaa1d
temptado <jason1287712@gmail.com>
2021-12-09 21:07:23 -0500

    added apollo provider into index file

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,38 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </>,
   document.getElementById('root')
 );
