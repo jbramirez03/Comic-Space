6903fd9fce9c691c1f9bc6451825a502671e58c2
Qabas <qabasalani1@gmail.com>
2025-06-08 09:46:24 +0700

    Fix: updated client config and Apollo setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,33 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-} from "@apollo/client";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
-import { split, HttpLink } from '@apollo/client';
-import { getMainDefinition } from '@apollo/client/utilities';
-import { WebSocketLink } from '@apollo/client/link/ws';
-import { Provider } from 'react-redux';
-import store from '../src/utils/store';
+import { split, HttpLink } from "@apollo/client";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { Provider } from "react-redux";
+import store from "../src/utils/store";
 
+// const wsLink = new WebSocketLink({
+//   uri: process.env.NODE_ENV === 'production'
+//     ? 'wss://young-hollows-20691.herokuapp.com/graphql'
+//     : 'ws://localhost:3001/graphql',
+//   options: {
+//     reconnect: true
+//   }
+// });
+
+// const httpLink = new HttpLink({
+//   uri: process.env.NODE_ENV === 'production'
+//     ? 'https://young-hollows-20691.herokuapp.com/graphql'
+//     : 'http://localhost:3001/graphql',
+// });
+
+// Replace '192.168.1.24' with your PC’s actual local IP if it's different
 const wsLink = new WebSocketLink({
-  uri: process.env.NODE_ENV === 'production'
-    ? 'wss://young-hollows-20691.herokuapp.com/graphql'
-    : 'ws://localhost:3001/graphql',
-  options: {
-    reconnect: true
-  }
+  uri: "ws://192.168.1.24:3001/graphql",
+  options: { reconnect: true },
 });
 
 const httpLink = new HttpLink({
-  uri: process.env.NODE_ENV === 'production'
-    ? 'https://young-hollows-20691.herokuapp.com/graphql'
-    : 'http://localhost:3001/graphql',
+  uri: "http://192.168.1.24:3001/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
@@ -40,13 +46,12 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
-
 const splitLink = split(
   ({ query }) => {
     const definition = getMainDefinition(query);
     return (
-      definition.kind === 'OperationDefinition' &&
-      definition.operation === 'subscription'
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
     );
   },
   wsLink,
@@ -66,7 +71,7 @@ ReactDOM.render(
       </Provider>
     </ApolloProvider>
   </>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 reportWebVitals();
